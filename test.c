91949607784b29b9f352489f70776a6d57855e15
Prashant Sinha <prashant+git@noop.pw>
2017-01-08 17:20:00 +0530

    Added MakeFile and updated tests

--- test.c
+++ test.c
@@ -9,17 +9,11 @@
 #include "src/ge.h"
 #include "src/sc.h"
 
+#include "external/cutest.h"
 
-int main() {
-    unsigned char public_key[32], private_key[64], seed[32], scalar[32];
-    unsigned char other_public_key[32], other_private_key[64];
-    unsigned char shared_secret[32], other_shared_secret[32];
+void test_signature_verify(void) {
+    unsigned char public_key[32], private_key[64], seed[32];
     unsigned char signature[64];
-
-    clock_t start;
-    clock_t end;
-    int i;
-
     const unsigned char message[] = "Hello, world!";
     const int message_len = strlen((char*) message);
 
@@ -31,11 +25,18 @@ int main() {
     ed25519_sign(signature, message, message_len, public_key, private_key);
 
     /* verify the signature */
-    if (ed25519_verify(signature, message, message_len, public_key)) {
-        printf("valid signature\n");
-    } else {
-        printf("invalid signature\n");
-    }
+    TEST_CHECK(ed25519_verify(signature, message, message_len, public_key));
+}
+
+void test_signature_scalar(void) {
+    unsigned char public_key[32], private_key[64], seed[32], scalar[32];
+    unsigned char signature[64];
+    const unsigned char message[] = "Hello, world!";
+    const int message_len = strlen((char*) message);
+
+    /* create a random seed, and a keypair out of that seed */
+    ed25519_create_seed(seed);
+    ed25519_create_keypair(public_key, private_key, seed);
 
     /* create scalar and add it to the keypair */
     ed25519_create_seed(scalar);
@@ -45,21 +46,29 @@ int main() {
     ed25519_sign(signature, message, message_len, public_key, private_key);
 
     /* verify the signature with the new keypair */
-    if (ed25519_verify(signature, message, message_len, public_key)) {
-        printf("valid signature\n");
-    } else {
-        printf("invalid signature\n");
-    }
+    TEST_CHECK(ed25519_verify(signature, message, message_len, public_key));
+}
 
-    /* make a slight adjustment and verify again */
+void test_signature_changes(void) {
+    unsigned char public_key[32], private_key[64], seed[32];
+    unsigned char signature[64];
+    const unsigned char message[] = "Hello, world!";
+    const int message_len = strlen((char*) message);
+
+    ed25519_create_seed(seed);
+    ed25519_create_keypair(public_key, private_key, seed);
+    ed25519_sign(signature, message, message_len, public_key, private_key);
+
     signature[44] ^= 0x10;
-    if (ed25519_verify(signature, message, message_len, public_key)) {
-        printf("did not detect signature change\n");
-    } else {
-        printf("correctly detected signature change\n");
-    }
+    TEST_CHECK(!ed25519_verify(signature, message, message_len, public_key));
+}
 
-    /* generate two keypairs for testing key exchange */
+void test_key_exchange(void) {
+    unsigned char public_key[32], private_key[64], seed[32];
+    unsigned char other_public_key[32], other_private_key[64];
+    unsigned char shared_secret[32], other_shared_secret[32];
+    int i;
+
     ed25519_create_seed(seed);
     ed25519_create_keypair(public_key, private_key, seed);
     ed25519_create_seed(seed);
@@ -71,80 +80,16 @@ int main() {
 
     for (i = 0; i < 32; ++i) {
         if (shared_secret[i] != other_shared_secret[i]) {
-            printf("key exchange was incorrect\n");
             break;
         }
     }
-
-    if (i == 32) {
-        printf("key exchange was correct\n");
-    }
-
-    /* test performance */
-    printf("testing seed generation performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_create_seed(seed);
-    }
-    end = clock();
-
-    printf("%fus per seed\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-
-    printf("testing key generation performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_create_keypair(public_key, private_key, seed);
-    }
-    end = clock();
-
-    printf("%fus per keypair\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    printf("testing sign performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_sign(signature, message, message_len, public_key, private_key);
-    }
-    end = clock();
-
-    printf("%fus per signature\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    printf("testing verify performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_verify(signature, message, message_len, public_key);
-    }
-    end = clock();
-
-    printf("%fus per signature\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-    
-
-    printf("testing keypair scalar addition performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_add_scalar(public_key, private_key, scalar);
-    }
-    end = clock();
-
-    printf("%fus per keypair\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    printf("testing public key scalar addition performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_add_scalar(public_key, NULL, scalar);
-    }
-    end = clock();
-
-    printf("%fus per key\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    printf("testing key exchange performance: ");
-    start = clock();
-    for (i = 0; i < 10000; ++i) {
-        ed25519_key_exchange(shared_secret, other_public_key, private_key);
-    }
-    end = clock();
-
-    printf("%fus per shared secret\n", ((double) ((end - start) * 1000)) / CLOCKS_PER_SEC / i * 1000);
-
-    return 0;
+    TEST_CHECK(i == 32);
 }
+
+TEST_LIST = {
+   { "Signature Verification", test_signature_verify },
+   { "Signature Scalar Addition", test_signature_scalar },
+   { "Signature Change", test_signature_changes },
+   { "Key Exchange", test_key_exchange },
+   { 0 }
+};
